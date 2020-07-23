6fc78b024dfac623d40560d2c0ea6d0d79d09e5c
theGalkin <53794828+thegalkin@users.noreply.github.com>
2020-07-23 22:07:52 +0300

    --- Auto-Git Commit ---

--- downloader.py
+++ downloader.py
@@ -1,21 +1,32 @@
-import requests as re
+import requests as requests
 from bs4 import BeautifulSoup as bs
 import time
 import pathlib as ph
+from os import chdir
 headers  = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,/;q=0.8', 'Accept-Encoding': 'gzip, deflate, sdch', 'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4', 'Cache-Control': 'max-age=0', 'Connection': 'close', 'Host': 'www.propertyguru.com.sg', 'Referer': 'propertyguru.com.sg/singapore-property-listing/…', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
-def downloadPage(link):
-    html = re.get(url=url, headers=headers)
+def downloadPage(url):
+    html = requests.get(url=url, headers=headers)
     html = bs(html, "html")
     name = html.title()
+    name = name[name.find("Вокзал ")+1:]
     with open("{}.html".format(name), "w+") as f:
         f.write(html)
 
-def listImporter():
-    linksFile = open("вокзалы.xlsx", "r")
-    return linksFile
 
-ph.Path.
-linksFile = listImporter()
+with open("terminals.txt", "r") as f:
+    linksFile = f.read()
+
+chdir("{}/downloads".format(ph.Path.cwd()))
+i = 0
+print("{} - тута".format(linksFile[:linksFile.find("\n")]))
+for link in linksFile:
+    i+=1
+    if i == 4: break
+    time.sleep(1)
+    downloadPage(link)
+    print("{i} - идет")
+
+
 
 
 
