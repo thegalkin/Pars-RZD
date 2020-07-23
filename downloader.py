d14d098ea64f25b9c4c8e8d359682e7a9f7b300a
theGalkin <53794828+thegalkin@users.noreply.github.com>
2020-07-24 01:42:25 +0300

    --- Auto-Git Commit ---

--- downloader.py
+++ downloader.py
@@ -3,14 +3,18 @@ from bs4 import BeautifulSoup as bs
 import time
 import pathlib as ph
 from os import chdir
-
+import sys
+from urllib.request import urlopen
+import re
 headers  = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,/;q=0.8', 'Accept-Encoding': 'gzip, deflate, sdch', 'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4', 'Cache-Control': 'max-age=0', 'Connection': 'close', 'Host': 'www.propertyguru.com.sg', 'Referer': 'propertyguru.com.sg/singapore-property-listing/…', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
 def downloadPage(url):
-    html = requests.get(url=url).text
-    html = bs(html, "lxml").prettify("WINDOWS-1252")
+    html = urlopen(url).read().decode("utf-8")
+    html = bs(html, "lxml")
     
     name = str(html.title.text)
-    #name = name[name.find("Вокзал ")+1:]
+    name = name.replace("  ", " ")
+    name = name[name.find(" ")+1:]
+    
     with open("{}.html".format(name), "w+") as f:
         f.write(str(html))
 
