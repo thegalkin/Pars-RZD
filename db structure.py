c77a1f4e740965ab5b7c3b660cf9201977e961c3
theGalkin <53794828+thegalkin@users.noreply.github.com>
2020-07-24 02:12:34 +0300

    --- Auto-Git Commit ---

--- db structure.py
+++ db structure.py
@@ -1,5 +1,16 @@
 from bs4 import BeautifulSoup as bs
 import sys, time, re
-
+import pathlib as ph
+from os import listdir,chdir
+from os.path import isfile, join
 def appendToDb():
     return
+
+chdir("{}/downloads".format(ph.Path.cwd()))
+onlyfiles = [f for f in listdir(ph.Path.cwd()) if isfile(join(ph.Path.cwd(), f))] # создаем список файлов в папке загрузок
+i = 0
+for fName in onlyfiles:
+    i+=1
+    if i == 2: break
+    with open(str(join(ph.Path.cwd(), fName)), "r") as site:
+        print(bs(site.read(), "lxml").prettify())
