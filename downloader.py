06a5cf62400a34eb8276b325ad56715e0141efd2
theGalkin <53794828+thegalkin@users.noreply.github.com>
2020-07-23 23:08:11 +0300

    --- Auto-Git Commit ---

--- downloader.py
+++ downloader.py
@@ -3,14 +3,18 @@ from bs4 import BeautifulSoup as bs
 import time
 import pathlib as ph
 from os import chdir
+
 headers  = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,/;q=0.8', 'Accept-Encoding': 'gzip, deflate, sdch', 'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4', 'Cache-Control': 'max-age=0', 'Connection': 'close', 'Host': 'www.propertyguru.com.sg', 'Referer': 'propertyguru.com.sg/singapore-property-listing/…', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
 def downloadPage(url):
-    html = requests.get(url=url).decode("WINDOWS-1251")
-    html = bs(html.text, "lxml")
+    html = requests.get(url=url)
+    html = str(html)
+    html = html.decode("WINDOWS-1251").encode("utf-8")
+    html = bs(html.text, "html")
+    html = str()
     name = str(html.title())
-    name = name[name.find("Вокзал ")+1:]
+    #name = name[name.find("Вокзал ")+1:]
     with open("{}.html".format(name), "w+") as f:
-        f.write(str(html))
+        f.write(html)
 
 
 linksFile = open("terminals.txt", "r")
