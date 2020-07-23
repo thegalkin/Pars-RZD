e1c4caa4a519113532b8116102e2dd96538d8042
theGalkin <53794828+thegalkin@users.noreply.github.com>
2020-07-24 02:42:43 +0300

    --- Auto-Git Commit ---

--- db structure.py
+++ db structure.py
@@ -6,11 +6,20 @@ from os.path import isfile, join
 def appendToDb():
     return
 
+
 chdir("{}/downloads".format(ph.Path.cwd()))
 onlyfiles = [f for f in listdir(ph.Path.cwd()) if isfile(join(ph.Path.cwd(), f))] # создаем список файлов в папке загрузок
 i = 0
+items = []
 for fName in onlyfiles:
     i+=1
     if i == 2: break
     with open(str(join(ph.Path.cwd(), fName)), "r") as site:
-        print(bs(site.read(), "lxml").prettify())
+        soup = bs(site.read(), "lxml")
+        for line in soup:
+            item = soup.find("td", class_="availabiItm").text
+            item = re.sub("^\s+|\n|\t|\s+$", '', item)
+            items.append(item)
+
+
+print(items)
