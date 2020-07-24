d0e7aedcddcac77b681f0455e87af7082550ac42
theGalkin <53794828+thegalkin@users.noreply.github.com>
2020-07-24 03:27:07 +0300

    пункты и проценты находит

--- db structure.py
+++ db structure.py
@@ -20,9 +20,15 @@ for fName in onlyfiles:
             item = soup.find("td", class_="availabiItm").text
             item = re.sub("^\s+|\n|\t|\s+$", '', item)
             items.append(item)"""
-        
+        print(soup.title.string)
         for tag in soup.findAll("td", class_="availabiItm"):
             
-            print("{0}: {1}: {}".format(tag.name, re.sub("\n|\t", '', tag.h3.string), tag.findAll(class_="percentRate pR-1")))
+            print("{0}: {1}: ".format(tag.name, re.sub("\n|\t", '', tag.h3.string) ), end="")
+            if tag.find(class_="percentRate pR-1"):
+                print(tag.find(class_="percentRate pR-1").string )
+            elif(tag.find(class_="percentRate pR-2") ):
+                print(tag.find(class_="percentRate pR-2").string )
+            elif tag.find(class_="percentRate pR-3") :
+                print(tag.find(class_="percentRate pR-3").string )
 
 print(items)
