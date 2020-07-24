0bdd68b88643d0d304619da6fb05433f4f0fad93
theGalkin <53794828+thegalkin@users.noreply.github.com>
2020-07-24 03:43:01 +0300

    --- Auto-Git Commit ---

--- db structure.py
+++ db structure.py
@@ -24,11 +24,13 @@ for fName in onlyfiles:
         for tag in soup.findAll("td", class_="availabiItm"):
             
             print("{0}: {1}: ".format(tag.name, re.sub("\n|\t", '', tag.h3.string) ), end="")
+
+            
+
+
             if tag.find(class_="percentRate pR-1"):
                 print(tag.find(class_="percentRate pR-1").string )
             elif(tag.find(class_="percentRate pR-2") ):
                 print(tag.find(class_="percentRate pR-2").string )
             elif tag.find(class_="percentRate pR-3") :
                 print(tag.find(class_="percentRate pR-3").string )
-
-print(items)
